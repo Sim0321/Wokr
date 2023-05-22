f50291b35b4ebe4e04abf637324d0c4f121cbed3
Sim <0doogadooga@gmail.com>
2023-05-22 10:55:23 +0900

    fix : 튜토리얼 이미지 수정 및 드롭다운 호버 색상 변경

--- src/components/global/globaldropdown/OkrDropDown.jsx
+++ src/components/global/globaldropdown/OkrDropDown.jsx
@@ -15,7 +15,7 @@ const OkrDropDown = ({ setKid, setOid, title }) => {
   const [isOpen, setIsOpen] = useDropDown(dropDownRef, false);
 
   const [valueState, setValueState] = useState(false);
-
+  //ㅇ
   const [finalValue, setFinalValue] = useState('');
 
   const [onFocus, setOnFocus] = useState(false);
